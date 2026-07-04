07b41d27ad74c8039880aa241531b345609535e2
dante-nl <dante@cyu.nl>
2026-07-04 23:02:46 +0200

    added linting

---
+++ eslint.config.js
@@ -0,0 +1,19 @@
+// eslint.config.js
+const { defineConfig } = require('eslint/config');
+const js = require('@eslint/js');
+const globals = require('globals');
+
+module.exports = defineConfig([
+    {
+        files: ['**/*.{js,mjs,cjs}'],
+        plugins: { js },
+        extends: ['js/recommended'],
+        languageOptions: {
+            globals: {
+                process: "readonly",
+                ...globals.browser
+            }
+        },
+    },
+    { files: ['**/*.js'], languageOptions: { sourceType: 'commonjs' } },
+]);
